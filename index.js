e5a7a5a381766c2d522597aee055d1abed0e8140
shailesh-choudhari <shailesh.choudhari@bnt-soft.com>
2017-11-07 16:41:32 +0530

    upated keywords

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const request = require('request-promise');
 
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
-const scopes = 'read_products';
+const scopes = 'read_orders, read_products';
 const forwardingAddress = "https://b2ed261e.ngrok.io"; // Replace this with your HTTPS Forwarding address
 
 app.get('/', (req, res) => {
